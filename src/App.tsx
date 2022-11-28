65784fdb177bf547516bfeb1cbb0b0b943a78198
@jardim <joao.jardim@syonet.com>
2022-11-28 19:48:16 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 export default App;
